13cbb0eb6557828f3a26c408d655d9372804b781
Øyvind Kristiansen <r3d5un@pm.me>
2024-03-17 18:23:11 +0100

    Connect to RabbitMQ

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/r3d5un/QueueToNull/internal/queue"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type application struct {
@@ -27,6 +29,27 @@ func main() {
 
 	slog.Info("starting application")
 
+	slog.Info("connecting to RabbitMQ")
+	conn, err := amqp.Dial("amqp://devUser:password@localhost:5672/my_vhost")
+	if err != nil {
+		logger.Error("unable to dial RabbitMQ", "error", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	mqPool, err := queue.NewChannelPool(conn, 10)
+	if err != nil {
+		logger.Error("unable to create RabbitMQ channel pool", "error", err)
+		os.Exit(1)
+	}
+
+	c, err := mqPool.GetChannel()
+	if err != nil {
+		logger.Error("unable to get channel from pool", "error", err)
+		os.Exit(1)
+	}
+	defer mqPool.ReturnChannel(c)
+
 	app := application{
 		logger: logger,
 	}
@@ -42,7 +65,7 @@ func main() {
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 
 	os.Exit(1)
