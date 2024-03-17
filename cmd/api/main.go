94a95723246f73311e5bec16f40be00f2d1a7e96
Øyvind Kristiansen <r3d5un@pm.me>
2024-03-17 14:00:24 +0100

    Added Air configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	handler := slog.NewJSONHandler(os.Stdout, nil)
+	logger := slog.New(handler)
+	instanceLogger := logger.With(
+		slog.Group(
+			"application_instance",
+			slog.String("instance_id", uuid.New().String()),
+		),
+	)
+	slog.SetDefault(instanceLogger)
+
+	slog.Info("starting application")
+
+	app := application{
+		logger: logger,
+	}
+
+	slog.Info("creating server")
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", 4000),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/r3d5un/QueueToNull
 
 go 1.22.0
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/justinas/alice v1.2.0
+)
